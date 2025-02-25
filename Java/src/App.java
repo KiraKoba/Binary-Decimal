60b0bb11d6a082285e7f8e622c3955f19f81707f
KiraKoba <fakira.kobayashi@gmail.com>
2025-02-25 15:36:13 -0300

    Bin2Dec Converter Java

--- Java/src/App.java
+++ Java/src/App.java
@@ -1,5 +1,30 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+
+class main{
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        boolean cont = true;
+
+        while (cont) {
+            System.out.println("Enter your binary:");
+            String binary = myScanner.nextLine();
+    
+            try{
+                int decimalNumber=Integer.parseInt(binary,2);
+                System.out.println(decimalNumber);
+                
+            }catch(NumberFormatException e){
+                System.out.println("Binary invalid.");
+            }
+
+            System.out.println("Do you wanna to convert another binary number? (y/n)");
+            String answer = myScanner.nextLine();
+
+            if (answer.equals("n") || answer.equals("N")) {
+                cont=false;
+                System.out.println("Program ended.");
+            }
+            
+        }
     }
 }
