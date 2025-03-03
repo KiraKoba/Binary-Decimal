b539a9e4284da4a66691a0b1b0cb2f57c3a3aca5
KiraKoba <fakira.kobayashi@gmail.com>
2025-03-03 00:21:52 -0300

    Add decimal convertor

--- C C++/b2dC.cpp
+++ C C++/b2dC.cpp
@@ -1,10 +1,11 @@
+#include <cctype>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <conio.h>
 
 int binConvertor(char *binStrg) {
-    int decimal=0;
+    int decimal = 0;
 
     while (*binStrg != '\0') {
         decimal = decimal * 2 + (*binStrg - '0');
@@ -13,12 +14,46 @@ int binConvertor(char *binStrg) {
     return decimal;
 }
 
+void decConvertor(int decimal, char *binStrg) {
+    int index = 0;
+
+    if (decimal == 0) {
+        binStrg[index] = '0';
+    } else {
+        while (decimal > 0) {
+            binStrg[index] = (decimal % 2) + '0';
+            decimal /= 2;
+            index++;
+        }
+    }
+    binStrg[index] = '\0';
+
+    int start = 0, end = index - 1;
+    while (start < end) {
+        char temp = binStrg[start];
+        binStrg[start] = binStrg[end];
+        binStrg[end] = temp;
+        start++;
+        end--;
+    }
+}
+
 bool isBinary(char *binStrg) {
     while (*binStrg != '\0') {
         if (*binStrg != '0' && *binStrg != '1') {
             return false;
         }
         binStrg++;
+    }
+    return true;
+}
+
+bool isDecimal(char *decimal) {
+    while (*decimal != '\0') {
+        if (!isdigit(*decimal)) {
+            return false;
+        }
+        decimal++;
     }
     return true;
 }
@@ -36,8 +71,8 @@ bool verifyYN(char choice) {
 }
 
 int main() {
-    int flag, loopFlag = 1, binDecResult = 0;
-    char binStrg[100], binaryFlag;
+    int flag, loopFlag = 1, binDecResult = 0, decInt;
+    char binStrg[100], decStrg[100], binaryFlag, decBinResult[100];
 
     do {
         system("cls");
@@ -53,7 +88,7 @@ int main() {
                 do {
                     system("cls");
                     printf("##### Binary to Decimal #####\n\n");
-                    while (1) {
+                    while (true) {
                         printf("Type your binary number:");
                         scanf("%99s", binStrg); //overflow protection;
 
@@ -73,14 +108,38 @@ int main() {
                         printf("Would you like to convert another number? (y/n):");
                         scanf(" %c", &binaryFlag);
                         printf("\n");
-                    }while (!verifyYN(binaryFlag));
-                }while (binaryFlag == 'y' || binaryFlag == 'Y');
+                    } while (!verifyYN(binaryFlag));
+                } while (binaryFlag == 'y' || binaryFlag == 'Y');
                 break;
 
             case 2:
-                system("cls");
-                printf("##### Decimal to Binary #####\n\n");
-                system("pause");
+                do {
+                    system("cls");
+                    printf("##### Decimal to Binary #####\n\n");
+
+                    while (true) {
+                        printf("Type your decimal number:");
+                        scanf("%99s", decStrg); //overflow protection;
+
+                        if (isDecimal(decStrg)) {
+                            break;
+                        }
+                        printf("\nInvalid decimal number! Try again.\n");
+                        getch();
+                    }
+                    decInt = atoi(decStrg);
+                    decConvertor(decInt, decBinResult);
+                    printf("The binary value is: %s\n", decBinResult);
+                    printf("\n\nPress any key to continue...");
+                    getch();
+
+                    do {
+                        system("cls");
+                        printf("Would you like to convert another number? (y/n):");
+                        scanf(" %c", &binaryFlag);
+                        printf("\n");
+                    } while (!verifyYN(binaryFlag));
+                } while (binaryFlag == 'y' || binaryFlag == 'Y');
                 break;
 
             case 3:
@@ -93,6 +152,9 @@ int main() {
             default:
                 break;
         }
-    } while (loopFlag == 1);
+    } while
+    (loopFlag
+     ==
+     1
+    );
 }
-
